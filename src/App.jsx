cf1440c1a02c3a4227d395cf53e801b6df3fd05b
aditya <adityaraj309050@gmail.com>
2025-03-26 12:45:15 +0530

    01

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/Header';
+const App = () => {
+  return (
+    <div >
+       <Header/>
+       <div className="text-red-700">
+      App
+      </div>
+      </div>
+    
+  );
+};
+
+export default App;
+
+
