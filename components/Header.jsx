d0acfb3d561f292db94d4a1b6912faa081e07f13
aditya <adityaraj309050@gmail.com>
2025-03-26 23:22:58 +0530

    0

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="bg-white text-black p-6 shadow-md rounded-full w-450 h-18 flex mt-12 ">
+    <div className="bg-white text-black p-6 shadow-md rounded-full w-450 h-18 flex mx-8  ">      
       <ul className="flex justify space-x-12   text-black ">
         <div className="text-orange-500">
         <li className="relative cursor-pointer hover:text-orange-500 after:block after:w-0 after:h-[3px] after:bg-orange-500 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
@@ -14,6 +14,14 @@ const Header = () => {
              Work</li>
              <li className="relative cursor-pointer hover:text-orange-500 after:block after:w-0 after:h-[3px] after:bg-orange-500 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
             About</li>
+            
+            <div className="flex justify-center absolute left-1/2 transform -translate-x-1/2">
+        <img 
+          src="https://media.licdn.com/dms/image/v2/C560BAQEOqhwqMgsGWQ/company-logo_200_200/company-logo_200_200/0/1635812365073?e=2147483647&v=beta&t=2TQxUoBNyAhPRXrRJvXo22yKRVit2mEwUf3Oft8wdN8"
+          alt="Company Logo"
+          className="w-30 h-30"
+        />
+      </div>
         </ul>
         <div className="ml-auto ">
         <li className=" list-none relative cursor-pointer hover:text-orange-500 after:block after:w-0 after:h-[3px] after:bg-orange-500 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full">
