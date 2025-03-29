f8e6bfb1a575f29af165d4da3fdbe3063795c1f4
aditya <adityaraj309050@gmail.com>
2025-03-29 21:52:14 +0530

    03

--- components/Body2.jsx
+++ components/Body2.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React from "react";
 
 const Body2 = () => {
   return (
-    <div className=" my-1">
-      <div style={{ backgroundColor: "#E8E5E2" }} className="w-full max-w-20xl h-100 rounded-lg shadow-lg flex items-center justify-center">
-
-        <p className="text-lg font-semibold  mt-[-4px] ">
-            <p className='text-orange-400'>
-                 smallscreen services </p>
-
-
-
+    <div className="my-1">
+      <div
+        className="w-full h-130 rounded-lg shadow-lg flex flex-col items-center p-10"
+        style={{ backgroundColor: "#E8E5E2" }}
+      >
+        <p className="font-semibold flex flex-col items-center text-center mt-10 w-full">
+          <span className="text-orange-600 text-4xl font-thin">SmallScreen Services</span>
+          <span className="text-5xl font-medium mt-15 text-gray-700">
+            Be <span className="text-orange-600">Creative</span>, We'll Handle the Rest
+          </span>
+          <span className="text-2xl font-thin mt-4">
+            SmallScreen offers world-class management services for world-class creators. We help elevate all <br />
+            brand partnerships, and everything in between to ensure that you reach the peak of your success.
+          </span>
         </p>
+
+       
+        <div className="w-full flex justify-start mt-18 relative -right-200">
+          <button className="px-8 py-2 bg-orange-600 text-white text-lg font-semibold rounded-full shadow-lg hover:bg-orange-700 transition duration-300">
+            Get in Touch
+          </button>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Body2;
--- components/photos.jsx
+++ components/photos.jsx
@@ -5,20 +5,20 @@ import "swiper/css/pagination";
 import { Pagination, Autoplay } from "swiper/modules";
 
 const imageList = [
-  "/C:/Users/rahul/Downloads/small-scale photo\\'s/4x5-charli-damelio.avif",
-  "/https://media-s3-us-east-1.ceros.com/forbes/images/2022/08/25/5024ce934ae8eb1628b952bad3cd1da8/4x5-charli-damelio.png?imageOpt=1&fit=bounds&width=1082",
+  "/4.png",
+  "/2.png",
   "/1.webp",
-  "/images/photo4.jpg",
-  "/images/photo5.jpg",
-   "/images/photo5.jpg",
-   "/images/photo5.jpg",
-   "/images/photo5.jpg",
-   "/images/photo5.jpg",
-   "/images/photo5.jpg",
-   "/images/photo5.jpg",
-   "/images/photo5.jpg",
-   "/images/photo5.jpg",
-   "/images/photo5.jpg",
+  "/3.png",
+  "/5.png",
+   "/6.png",
+   "/7.png",
+   "/8.png",
+   "/9.png",
+   "/10.png",
+   "/11.png",
+   "/12.png",
+   "/13.png",
+   "/14.png",
 
 
 ];
